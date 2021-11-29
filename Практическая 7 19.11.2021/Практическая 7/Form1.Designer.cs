e2449692bced3809dd3ff5d6f0461d05bbc12ab8
Егор <rgor33334@gmail.com>
2021-11-29 14:20:07 +0700

    jnjnj

---
+++ Практическая 7 19.11.2021/Практическая 7/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Практическая_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(559, 232);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 250);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(256, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(627, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "максимальное ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ФИО";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Улица ";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Дом";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Квартира ";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(139, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 77);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "минимальное ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(139, 334);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Дом";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 334);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 21);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "квартра ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Практическая 7 19.11.2021/Практическая 7/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Практическая_7
+{
+    public partial class Form1 : Form
+    {
+        public struct person
+        {
+            public string FIO;
+            public string ul;
+            public int dom;
+            public int kv;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+                person[] mas = new person[n];
+                for (int i = 0; i < n; i++)
+                {
+                    mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                    mas[i].ul = dataGridView1[1, i].Value.ToString();
+                    mas[i].dom = Convert.ToInt32(dataGridView1[2, i].Value);
+                    mas[i].kv = Convert.ToInt32(dataGridView1[3, i].Value);
+                }
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, true, System.Text.Encoding.Default);
+                for (int i = 0; i < n; i++)
+                {
+                    sw.WriteLine(mas[i].FIO);
+                    sw.WriteLine(mas[i].ul);
+                    sw.WriteLine(mas[i].dom);
+                    sw.WriteLine(mas[i].kv);
+                }
+                sw.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            person[] mas = new person[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                mas[i].ul = dataGridView1[1, i].Value.ToString();
+                mas[i].dom = Convert.ToInt32(dataGridView1[2, i].Value);
+                mas[i].kv = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            if (checkBox1.Checked == true && checkBox2.Checked==true)
+            {
+                int max = 0;
+                for(int i = 0;i<n;i++)
+                {
+                    if (max < mas[i].dom)
+                    {
+                        max = mas[i].dom;
+                    }
+                }
+                int max2 = 0;
+                for (int i = 0; i < n ; i++)
+                {
+                    if (max2 < mas[i].kv)
+                    {
+                        max2 = mas[i].kv;
+                    }
+                }
+                MessageBox.Show("Дом с саммый большим номером:"+max+ "\r\n"+
+                                "Квартира с самым большим номером"+max2);    
+            }
+            else if(checkBox1.Checked == true)
+            {
+                int max = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (max < mas[i].dom)
+                    {
+                        max = mas[i].dom;
+                    }
+                }
+                MessageBox.Show("Дом с саммый большим номером:" + max);
+            }
+            else if(checkBox2.Checked == true)
+            {
+                int max = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (max < mas[i].kv)
+                    {
+                        max = mas[i].kv;
+                    }
+                }
+                MessageBox.Show("Квартира с саммый большим номером:" + max);
+            }
+
+            
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+                person[] mas = new person[n];
+                for (int i = 0; i < n; i++)
+                {
+                    mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                    mas[i].ul = dataGridView1[1, i].Value.ToString();
+                    mas[i].dom = Convert.ToInt32(dataGridView1[2, i].Value);
+                    mas[i].kv = Convert.ToInt32(dataGridView1[3, i].Value);
+                }
+                StreamReader sr = new StreamReader(openFileDialog1.FileName,System.Text.Encoding.Default);
+                int k = 0;
+                while (!sr.EndOfStream)
+                {
+                    k++;
+                    Array.Resize(ref mas, k);
+                    mas[k - 1].FIO = sr.ReadLine();
+                    mas[k - 1].ul = sr.ReadLine();
+                    mas[k - 1].dom = Convert.ToInt32(sr.ReadLine());
+                    mas[k - 1].kv = Convert.ToInt32(sr.ReadLine());
+                }
+                dataGridView1.RowCount = k;
+                numericUpDown1.Value = k;
+                sr.Close();
+                for(int i = 0; i < k; i++)
+                {
+                    dataGridView1[0, i].Value = mas[i].FIO;
+                    dataGridView1[1, i].Value = mas[i].ul;
+                    dataGridView1[2, i].Value = mas[i].dom;
+                    dataGridView1[3, i].Value = mas[i].kv;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            person[] mas = new person[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                mas[i].ul = dataGridView1[1, i].Value.ToString();
+                mas[i].dom = Convert.ToInt32(dataGridView1[2, i].Value);
+                mas[i].kv = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            if(checkBox1.Checked == true && checkBox2.Checked == true)
+            {
+                int min = 11111110;
+                for (int i = 0; i < n; i++)
+                {
+                    if (min> mas[i].dom)
+                    {
+                        min = mas[i].dom;
+                    }
+                    
+                }
+                int min2 = 1111110;
+                for (int i = 0; i < n ; i++)
+                {
+                    if (min2 > mas[i].kv)
+                    {
+                        min2 = mas[i].kv;
+                    }
+                    
+                }
+
+                MessageBox.Show("Дом с минимальным номером:" + min + "\r\n" +
+                                 "Квартира с мнимальным номером" + min2);
+            }
+            else if(checkBox1.Checked == true)
+            {
+                int min = 1111110;
+                for (int i = 0; i < n; i++)
+                {
+                    if (min > mas[i].dom)
+                    {
+                        min = mas[i].dom;
+                    }
+                    
+                }
+                MessageBox.Show("Дом с минимальным номером:" + min);
+            }
+            else if(checkBox2.Checked == true)
+            {
+                int min2 = 100000;
+                for (int i = 0; i < n; i++)
+                {
+                    if (min2 > mas[i].kv)
+                    {
+                        min2 = mas[i].kv;
+                    }
+                    
+                }
+                MessageBox.Show("Квартира  с минимальным номером:" + min2);
+                
+
+            }
+        }
+    }
+}
